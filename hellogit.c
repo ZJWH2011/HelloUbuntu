662fb55ceb152e5bb1374e424d7855f0da28b24f
lujy <821185277@qq.com>
2018-07-13 00:40:20 -0700

    add fork

--- hellogit.c
+++ hellogit.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc,char **argv)
 {
 	printf("hello git!\n");
 	printf("hello lujy!\n");
 	printf("push github!\n");
+
+	pid_t pid = fork();
+
+	int a=10;
+
+	if(-1 == pid)
+	{
+		printf("fork error!\n");
+	}
+	else if(0==pid)
+	{
+
+		a++;
+
+		printf("this is child,pid=%d,ppid=%d,a=%d\n",getpid(),getppid(),a);
+
+		exit(0);
+	}
+	else
+	{
+		a+=10;
+
+		printf("this is father,pid=%d,a=%d\n",getpid(),a);
+	}
 
 	return 0;
 }
